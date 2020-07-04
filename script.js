122258e6d432878ff3bde2b2c441c46a4ae8238f
varnasiuday <67265626+varnasiuday@users.noreply.github.com>
2020-07-04 11:27:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -72,6 +72,71 @@ function evaluate(input){
 	}
 }
  */
+function formValidation()
+{
+var uname = document.registration.username;
+var uemail = document.registration.email;
+var phoneno =document.registration.phnnumber;
+{
+if(allLetter(uname))
+{
+if(ValidateEmail(uemail))
+{
+if(allnumeric(phoneno))
+{
+}
+}
+}
+}
+return false;
+}
+function allLetter(uname)
+{ 
+var letters = /^[A-Za-z]+$/;
+if(uname.value.match(letters))
+{
+return true;
+}
+else
+{
+alert('Username must have alphabet characters only');
+uname.focus();
+return false;
+}
+}
+function ValidateEmail(uemail)
+{
+var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+if(uemail.value.match(mailformat))
+{
+return true;
+}
+else
+{
+alert("You have entered an invalid email address!");
+uemail.focus();
+return false;
+}
+}function allnumeric(phoneno)
+{
+var phoneno=/^\d{10}$/;
+if(phoneno.value.match(phoneno))
+{
+return true;
+}
+else
+{
+alert("you have entered an invalid phone number");
+phoneno.focus();
+return false;
+}
+else
+{
+alert('Form Succesfully Submitted');
+window.location.reload()
+return true;
+}
+}
 
 
 
